c2861bbe32af33906e4674959f6598ceed5fa3b9
codingclub007 <92330498+codingclub007@users.noreply.github.com>
2021-10-19 16:10:31 +0530

    primenumber.cpp

---
+++ contributions/contri.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int low, high, temp, i;
+    bool isPrime;
+    
+    cout << "Enter two numbers (intevals): ";
+    cin >> low >> high;
+
+    //swapping numbers if low is greater than high
+    if (low > high) {
+        temp = low;
+        low = high;
+        high = temp;
+    }
+    cout << "\nPrime numbers between " << low << " and " << high << " are:" << endl;
+
+    while (low < high) {
+        isPrime = true;
+        if (low == 0 || low == 1) {
+            isPrime = false;
+        }
+        else {
+            for (i = 2; i <= low / 2; ++i) {
+                if (low % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime)
+            cout << low << " ";
+
+        ++low;
+    }
+    return 0;
+}
